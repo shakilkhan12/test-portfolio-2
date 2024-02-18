07720014361ecf861fe45a641dde9f6ee2509392
shakilkhan12 <shakilkhan621@gmail.com>
2024-02-18 10:23:39 +0500

    fixed

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,9 @@
 "use client";
 import Provider from "@/components/Provider";
 import "./globals.css";
-import { useCallback } from "react";
-import Particles from "@tsparticles/react";
-import { loadSlim } from "@tsparticles/slim";
 import Three from "@/components/Three";
 
 export default function RootLayout({ children }) {
-  const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
-
-    await loadSlim(engine);
-  }, []);
-
-  const particlesLoaded = useCallback(async (container) => {
-    console.log(container);
-  }, []);
   return (
     // <ServerThemeProvider>
     <html lang="en">
